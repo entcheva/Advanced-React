f0b2d6a43546e1a993bff1027a0433544f5c021c
Christina Entcheva <christina@thoughtbot.com>
2020-01-03 20:13:40 -0500

    Add home and sell pages

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Sell = props => (
+    <div>
+        <p>get that money</p>
+
+        <Link href="/">
+          <a>home plz</a>
+        </Link>
+    </div>
+)
+
+export default Sell;
