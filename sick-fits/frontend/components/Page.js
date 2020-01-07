6fad883db34dd85869f11e87c5723e0884801dfd
Christina Entcheva <christina.entcheva@gmail.com>
2020-01-07 15:54:11 -0500

    Remove styled component

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
-import styled from 'styled-components';
-
-const MyButton = styled.button`
-  background: red;
-  font-size: 100px;
-`;
-// backticks are a tag template literal - works similarly to a function button()
 
 class Page extends Component {
   render() {
     return (
       <div>
         <Header />
-        <MyButton>Click Me</MyButton>
         {this.props.children}
         </div>
     );
